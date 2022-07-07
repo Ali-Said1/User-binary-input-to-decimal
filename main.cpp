d17a3ca4478d6353a93bdce633a7dbd943b76e56
Ali-Said1 <asazizg1@gmail.com>
2022-07-07 21:34:07 +0200

    added comments to explain the code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+
+//standard library needed for string type data
 #include <string>
+
+//standard library used for the pow Function
 #include <math.h>
 
 using namespace std;
@@ -10,8 +14,10 @@ int main(){
     std::cin >> user_input;
     bool valid_binary;
 
+//Validating that user input is binary by confirming it onlly contains zeroes and ones
     for(int i = 0; i < user_input.length(); i++){
         if(user_input[i] == '1' || user_input[i] == '0'){
+            //setting bool valid_binary to true to be checked on in line 30
             valid_binary = true;
         }
         else{
@@ -24,14 +30,15 @@ int main(){
     if(valid_binary == 1){
         int j = 0;
         std::string reversed;
+        //reversing the user input to be able to do the math for it easily
         for(int i = user_input.length() - 1; i >= 0; i--){
             reversed.push_back(user_input[i]);
             j++;
         }
         int result {0};
+        //looping the reversed user input to add the value to result variable
         for(int i = 0; i < reversed.length(); i++){
             result += (reversed[i] -48) * pow(2, i);
-            int x = reversed[i] * pow(2, i);
             std::cout << reversed[i] - 48 << " * " << pow(2, i) << " = " << (reversed[i] - 48) * pow(2, i)<< std::endl;
         }
         std::cout << "The binary: 0b" << user_input << " in decimal form is: " << result;
